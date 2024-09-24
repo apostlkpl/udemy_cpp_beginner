1b72fceb8f345d37488aa3abea77872e652a8e1a
Apostolos Kopalidis <apostkpl@gmail.com>
2024-09-24 18:59:58 +0300

    section 1

---
+++ section_1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+        int num;
+        std::cout << "Pick your favorite number between 1 and 100 : " << std::endl;
+        std::cin >> num;
+        std::cout << "Amazing!! That is my favorite number too!\nNo really!!, " << num << " is my favorite number!" << std::endl;
+}
